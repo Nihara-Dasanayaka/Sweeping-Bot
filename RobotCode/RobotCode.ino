340a158061e268bd7398af05672c5059c68122c9
CloudyPadmal <blog.padmal@gmail.com>
2017-01-30 06:24:26 +0530

    Improved code

--- RobotCode/RobotCode.ino
+++ RobotCode/RobotCode.ino
@@ -2,7 +2,7 @@
 #include <Motor.h>
 #include <Buzzer.h>
 
-// Constants for Sonar
+/*********************************** Sonar ************************************/
 #define sonarA_ECHO     44
 #define sonarA_TRIG     45
 #define sonarB_ECHO     46
@@ -13,7 +13,6 @@
 #define sonarD_TRIG     51
 #define sonarE_ECHO     52
 #define sonarE_TRIG     53
-// Sonar Extras
 #define sonarA_NTFY     23
 #define sonarB_NTFY     25
 #define sonarC_NTFY     27
@@ -21,40 +20,102 @@
 #define sonarE_NTFY     31
 #define MAX_DISTANCE    3000
 #define SOUND_FACTOR    59
-
-// Constants for Motors
-#define L_Motor_A       3
-#define L_Motor_B       4
-#define R_Motor_A       5
-#define R_Motor_B       6
+const int DANGER_ZONE = 10;
+/*********************************** Motor ************************************/
+#define L_Motor_A       4
+#define L_Motor_B       3
+#define R_Motor_A       6
+#define R_Motor_B       5
 #define L_Motor_EN      2
 #define R_Motor_EN      7
 #define L_Motor_Speed   150
 #define R_Motor_Speed   150
-
-// Constants for Buzzer
+/********************************** Buzzer ************************************/
 #define Buzzer_Pin      8
 
-// Sonar Sensors
+/******************************************************************************/
+/********************************** Sonars ************************************/
+/******************************************************************************/
 Sonar sonarA(sonarA_ECHO, sonarA_TRIG, MAX_DISTANCE, SOUND_FACTOR, sonarA_NTFY);
 Sonar sonarB(sonarB_ECHO, sonarB_TRIG, MAX_DISTANCE, SOUND_FACTOR, sonarB_NTFY);
 Sonar sonarC(sonarC_ECHO, sonarC_TRIG, MAX_DISTANCE, SOUND_FACTOR, sonarC_NTFY);
 Sonar sonarD(sonarD_ECHO, sonarD_TRIG, MAX_DISTANCE, SOUND_FACTOR, sonarD_NTFY);
 Sonar sonarE(sonarE_ECHO, sonarE_TRIG, MAX_DISTANCE, SOUND_FACTOR, sonarE_NTFY);
-// Motors
+/******************************************************************************/
+/********************************** Motors ************************************/
+/******************************************************************************/
 Motor leftMotor(L_Motor_A, L_Motor_B, L_Motor_EN, L_Motor_Speed);
 Motor rightMotor(R_Motor_A, R_Motor_B, R_Motor_EN, R_Motor_Speed);
-// Buzzer
+/******************************************************************************/
+/********************************** Buzzer ************************************/
+/******************************************************************************/
 Buzzer buzzer(Buzzer_Pin);
+
+/******************************************************************************/
+/********************************* Templates **********************************/
+/******************************************************************************/
+void scanArea();
+int calculateSpeed();
+void robotMove(int Speed);
+void avoidObstacles();
+
+/******************************************************************************/
+/********************************* Variables **********************************/
+/******************************************************************************/
+short surrounding[5] = {0, 0, 0, 0, 0};
+int leftMotorSpeed = 0, rightMotorSpeed = 0;
 
 void setup() {
   delay(100);
   buzzer.startTone();
-  // Turn on some decorative lights
+  delay(100);
 }
 
 void loop() {
-  sonarA.near(10);
+  // Scan the surroundings
+  scanArea();
+  // Calculate speed accordingly
+  robotMove(calculateSpeed());
+  avoidObstacles();
+  delay(10);
+}
+
+/*
+ An array of 1's and 0's will be created according to position
+*/
+void scanArea() {
+  surrounding[0] = sonarA.near(DANGER_ZONE) ? 1 : 0;
+  surrounding[1] = sonarB.near(DANGER_ZONE) ? 1 : 0;
+  surrounding[2] = sonarC.near(DANGER_ZONE) ? 1 : 0;
+  surrounding[3] = sonarD.near(DANGER_ZONE) ? 1 : 0;
+  surrounding[4] = sonarE.near(DANGER_ZONE) ? 1 : 0;
+}
+
+/*
+ The speed will be calculated according to the position
+*/
+int calculateSpeed() {
+  int SPEED = 0;
+  for (int i = 0; i < 5; i++) {
+    SPEED += surrounding[i] * (i + 1) * 10;
+  }
+  return SPEED;
+}
+
+void robotMove(int Speed) {
+  leftMotor.speedUp(Speed);
+  rightMotor.speedUp(Speed);
+}
+
+void avoidObstacles() {
+  if (surrounding[2] == 1) {
+    leftMotor.stopNow();
+    rightMotor.stopNow();
+  }
+}
+
+/*
+sonarA.near(10);
   sonarB.near(10);
   sonarC.near(10);
   sonarD.near(10);
@@ -90,4 +151,5 @@ void loop() {
   sonarD.near(10);
   sonarE.near(10);  
   delay(100);*/
-}
+
+
