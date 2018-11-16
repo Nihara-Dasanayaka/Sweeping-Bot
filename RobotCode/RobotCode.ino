47c1d04efee54605a0d2fd8c29cf5ed4d1c03f9f
CloudyPadmal <blog.padmal@gmail.com>
2018-11-16 13:15:44 +0530

    Added IMU

--- RobotCode/RobotCode.ino
+++ RobotCode/RobotCode.ino
@@ -1,38 +1,38 @@
-#include <Sonar.h>
 #include <Motor.h>
+#include <Sonar.h>
 #include <Buzzer.h>
+#include <Wire.h>
 
 /*********************************** Sonar ************************************/
-#define sonarA_ECHO     44
-#define sonarA_TRIG     45
-#define sonarB_ECHO     46
-#define sonarB_TRIG     47
-#define sonarC_ECHO     48
-#define sonarC_TRIG     49
-#define sonarD_ECHO     50
-#define sonarD_TRIG     51
-#define sonarE_ECHO     52
-#define sonarE_TRIG     53
-#define sonarA_NTFY     23
-#define sonarB_NTFY     25
-#define sonarC_NTFY     27
-#define sonarD_NTFY     29
-#define sonarE_NTFY     31
+#define sonarA_ECHO     53
+#define sonarA_TRIG     52
+#define sonarB_ECHO     51
+#define sonarB_TRIG     50
+#define sonarC_ECHO     49
+#define sonarC_TRIG     48
+#define sonarD_ECHO     47
+#define sonarD_TRIG     46
+#define sonarE_ECHO     45
+#define sonarE_TRIG     44
+#define sonarA_NTFY     36
+#define sonarB_NTFY     37
+#define sonarC_NTFY     38
+#define sonarD_NTFY     39
+#define sonarE_NTFY     40
 #define MAX_DISTANCE    3000
 #define SOUND_FACTOR    59
 const int DANGER_ZONE = 10;
 /*********************************** Motor ************************************/
-#define L_Motor_A       4
-#define L_Motor_B       3
-#define R_Motor_A       6
-#define R_Motor_B       5
-#define L_Motor_EN      2
-#define R_Motor_EN      7
+#define L_Motor_A       30
+#define L_Motor_B       31
+#define R_Motor_A       33
+#define R_Motor_B       32
+#define L_Motor_EN      8
+#define R_Motor_EN      9
 #define L_Motor_Speed   150
 #define R_Motor_Speed   150
 /********************************** Buzzer ************************************/
-#define Buzzer_Pin      8
-
+#define Buzzer_Pin      10
 /******************************************************************************/
 /********************************** Sonars ************************************/
 /******************************************************************************/
@@ -50,7 +50,11 @@ Motor rightMotor(R_Motor_A, R_Motor_B, R_Motor_EN, R_Motor_Speed);
 /********************************** Buzzer ************************************/
 /******************************************************************************/
 Buzzer buzzer(Buzzer_Pin);
-
+/******************************************************************************/
+/********************************** Buzzer ************************************/
+/******************************************************************************/
+const int MPU = 0x68;
+int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
 /******************************************************************************/
 /********************************* Templates **********************************/
 /******************************************************************************/
@@ -58,6 +62,8 @@ void scanArea();
 void robotMove();
 void avoidObstacles();
 void turnAround(bool RIGHT);
+void configureMPU();
+void readMPU();
 
 /******************************************************************************/
 /********************************* Variables **********************************/
@@ -68,16 +74,46 @@ int leftMotorSpeed = 0, rightMotorSpeed = 0;
 void setup() {
   delay(100);
   buzzer.startTone();
-  delay(100);
+  delay(200);
+  buzzer.alertTone();
+  delay(200);
+  configureMPU();
+  Serial.begin(9600);
 }
 
 void loop() {
   // Scan the surroundings
-  scanArea();
+  /*
+  long a = sonarA.distance();
+  Serial.print("A is - ");
+  Serial.print(a);
+  long b = sonarB.distance();
+  Serial.print("  B is - ");
+  Serial.print(b);
+  long c = sonarC.distance();
+  Serial.print("  C is - ");
+  Serial.print(c);
+  long d = sonarD.distance();
+  Serial.print("  D is - ");
+  Serial.print(d);
+  long e = sonarE.distance();
+  Serial.print("  E is - ");
+  Serial.println(e);
+  delay(100);*/
+  
+  //scanArea();
   // Avoid any obstacles
-  avoidObstacles();
+  //avoidObstacles();
   // Move straight
-  robotMove();
+  leftMotor.speedUp(100);
+  rightMotor.speedUp(100);
+  readMPU();  
+  Serial.print("Tmp = "); Serial.print(Tmp);
+  Serial.print(" | GyX = "); Serial.print(GyX);
+  Serial.print(" | GyY = "); Serial.print(GyY);
+  Serial.print(" | GyZ = "); Serial.println(GyZ);
+  delay(100);
+  //robotMove();
 }
 
 /*
@@ -132,3 +168,26 @@ void turnAround(bool RIGHT) {
   rightMotor.stopNow();
   delay(10);
 }
+
+void configureMPU() {
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+}
+
+void readMPU() {
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 14, true);
+  AcX = (Wire.read() << 8 | Wire.read()) / 16384;   
+  AcY = (Wire.read() << 8 | Wire.read()) / 16384;
+  AcZ = (Wire.read() << 8 | Wire.read()) / 16384;
+  Tmp = ((Wire.read() << 8 | Wire.read()) / 340.00) + 36.53;
+  GyX = (Wire.read() << 8 | Wire.read()) / 131;
+  GyY = (Wire.read() << 8 | Wire.read()) / 131;
+  GyZ = (Wire.read() << 8 | Wire.read()) / 131;
+}
+
