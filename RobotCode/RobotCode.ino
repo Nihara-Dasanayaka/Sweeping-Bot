12918da6363f7e57f6190152f2abadec4d472221
CloudyPadmal <blog.padmal@gmail.com>
2018-11-16 17:46:58 +0530

    added logics

--- RobotCode/RobotCode.ino
+++ RobotCode/RobotCode.ino
@@ -33,6 +33,7 @@ const int DANGER_ZONE = 10;
 #define R_Motor_Speed   150
 /********************************** Buzzer ************************************/
 #define Buzzer_Pin      10
+
 /******************************************************************************/
 /********************************** Sonars ************************************/
 /******************************************************************************/
@@ -51,10 +52,13 @@ Motor rightMotor(R_Motor_A, R_Motor_B, R_Motor_EN, R_Motor_Speed);
 /******************************************************************************/
 Buzzer buzzer(Buzzer_Pin);
 /******************************************************************************/
-/********************************** Buzzer ************************************/
+/*********************************** MPU **************************************/
 /******************************************************************************/
 const int MPU = 0x68;
-int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+const int MIN_GYRO = -3;
+const int MAX_GYRO = 3;
+const int MAX_TEMP = 40;
+
 /******************************************************************************/
 /********************************* Templates **********************************/
 /******************************************************************************/
@@ -62,26 +66,53 @@ void scanArea();
 void robotMove();
 void avoidObstacles();
 void turnAround(bool RIGHT);
+void initiateVariables();
 void configureMPU();
 void readMPU();
+bool isStuck();
+bool overHeating();
 
 /******************************************************************************/
 /********************************* Variables **********************************/
 /******************************************************************************/
 short surrounding[5] = {0, 0, 0, 0, 0};
 int leftMotorSpeed = 0, rightMotorSpeed = 0;
+int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+int16_t Gx[4] = {0, 0, 0, 0};
+int16_t Gy[4] = {0, 0, 0, 0};
+int16_t Gz[4] = {0, 0, 0, 0};
 
 void setup() {
+  // Play start tone
   delay(100);
   buzzer.startTone();
   delay(200);
   buzzer.alertTone();
   delay(200);
+  // Setup communication
   configureMPU();
+  // Setup logging  
   Serial.begin(9600);
 }
 
 void loop() {
+  // Check if the system is too hot
+  readMPU();
+  if (!overHeating()) {
+    Serial.println("I am cool!");
+    if (!isStuck) {
+      Serial.println("I am freee!");
+      // Scan surrounding area and move
+    } else {
+      Serial.println("I am stuck!");
+      // Move a bit away
+    }
+  } else {
+    Serial.println("I am overheating!");
+    leftMotor.stopNow();
+    rightMotor.stopNow();
+    delay(500);
+  }
   // Scan the surroundings
   /*
   long a = sonarA.distance();
@@ -105,13 +136,9 @@ void loop() {
   // Avoid any obstacles
   //avoidObstacles();
   // Move straight
-  leftMotor.speedUp(100);
-  rightMotor.speedUp(100);
-  readMPU();  
-  Serial.print("Tmp = "); Serial.print(Tmp);
-  Serial.print(" | GyX = "); Serial.print(GyX);
-  Serial.print(" | GyY = "); Serial.print(GyY);
-  Serial.print(" | GyZ = "); Serial.println(GyZ);
+  leftMotor.speedUp(200);
+  rightMotor.speedUp(200);
+  Serial.println();Serial.println();Serial.println();
   delay(100);
   //robotMove();
 }
@@ -169,6 +196,9 @@ void turnAround(bool RIGHT) {
   delay(10);
 }
 
+/**
+ * Set up communication between MPU and Arduino
+ */
 void configureMPU() {
   Wire.begin();
   Wire.beginTransmission(MPU);
@@ -177,11 +207,16 @@ void configureMPU() {
   Wire.endTransmission(true);
 }
 
+/**
+ * Take readings from the IMU
+ */
 void readMPU() {
+  // Initiate communication
   Wire.beginTransmission(MPU);
   Wire.write(0x3B);
   Wire.endTransmission(false);
   Wire.requestFrom(MPU, 14, true);
+  // Read register values
   AcX = (Wire.read() << 8 | Wire.read()) / 16384;   
   AcY = (Wire.read() << 8 | Wire.read()) / 16384;
   AcZ = (Wire.read() << 8 | Wire.read()) / 16384;
@@ -189,5 +224,74 @@ void readMPU() {
   GyX = (Wire.read() << 8 | Wire.read()) / 131;
   GyY = (Wire.read() << 8 | Wire.read()) / 131;
   GyZ = (Wire.read() << 8 | Wire.read()) / 131;
+  // Fill out the arrays
+  fillArray(GyX, Gx);
+  Serial.print("Gx = ");
+  printArray(Gx);
+  fillArray(GyY, Gy);
+  Serial.print("Gy = ");
+  printArray(Gy);
+  fillArray(GyZ, Gz);
+  Serial.print("Gz = ");
+  printArray(Gz);
 }
 
+void fillArray(int16_t value, int16_t arr) {
+  arr[0] = arr[1];
+  arr[1] = arr[2];
+  arr[2] = arr[3];
+  arr[3] = value;
+}
+
+void printArray(int16_t arr) {
+  Serial.print("{");
+  for (int i = 0; i < 4; i++) {
+    Serial.print(arr[i]);
+    if (i != 3) {
+      Serial.print(", ");
+    }
+  }
+  Serial.println("}");
+}
+
+/**
+ * Checks whether the robot is stalling for a while
+ */
+bool isStuck() {
+  if (checkElements(Gx) && checkElements(Gy) && checkElements(Gz)) {
+    if (
+      (Gx[0] < MAX_GYRO && Gx[0] > MIN_GYRO) && 
+      (Gy[0] < MAX_GYRO && Gy[0] > MIN_GYRO) && 
+      (Gz[0] < MAX_GYRO && Gz[0] > MIN_GYRO)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
+ * Checks if all the elements in the array are close by values
+ */
+bool checkElements(int arr[]) {
+  int THRESHOLD = 1;
+  int normal = 0;
+  for (int n = 0; n < 4; n++) {
+    normal += arr[n];
+  }
+  normal = (normal / 4) - arr[0];
+  return (normal < THRESHOLD);
+}
+
+/**
+ * Checks if the internal of robot is getting too hot
+ */
+bool overHeating() {
+  if (Tmp > MAX_TEMP) {
+    Serial.print("Temperature is ");
+    Serial.println(Tmp);
+    buzzer.alertTone();
+    return true;
+  } else {
+    return false;
+  }
+}
